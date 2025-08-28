569fa2c2812b4f7909c9ca930b380b981c38e144
RobinPicard <robin.picard@sciencespo.fr>
2025-08-28 15:04:36 +0200

    Simplify the plugin, add tests and documentation

--- langextract_outlines/__init__.py
+++ langextract_outlines/__init__.py
@@ -1,6 +1,6 @@
 """LangExtract provider plugin for Outlines."""
 
-from langextract_outlines.provider import OutlinesStructuredGenerationModel
+from langextract_outlines.provider import OutlinesProvider
 
-__all__ = ["OutlinesStructuredGenerationModel"]
+__all__ = ["OutlinesProvider"]
 __version__ = "0.1.0"
